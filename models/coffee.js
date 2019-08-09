5ea76876702b40010a5090a409aab7db99ac236c
John Nguyen <Johnsum41.1@gmail.com>
2019-08-09 13:37:59 -0400

    coffee

--- models/coffee.js
+++ models/coffee.js
@@ -1,13 +1,33 @@
-const pgp = require('pg-promise')({
-    query: e => {
-        console.log('QUERY ', e.query);
+const db = require('./conn');
+
+class Coffee {
+    constructor(id, name, bespoke, size){
+        this.id = id;
+        this.name = name;
+        this.bespoke = bespoke;
+        this.size = size;
     }
-})
-
-const options = {
-    host: 'localhost',
-    database: 'coffee-orders-app'
-};
-
-const db = pgp(options);
-module.exports = db;
+    static getById(id){
+        return db.one(`select * from coffee where id = ${id}`)
+            .then((oneCoffeeData)=>{
+                const coffeeInstance = new coffee(oneCoffeeData.id,
+                    oneCoffeeData.name,
+                    oneCoffeeData.bespoke,
+                    oneCoffeeData.size
+                    );
+                return coffeeInstance;
+            })
+            .catch(() => {
+                return null;
+            });
+    }
+    save(){
+        return db.result(`
+        update coffee set 
+        name = ${this.name},
+        bespoke = ${this.bespoke},
+        size = ${this.size},
+        where id = ${this.id}
+        `)
+    }
+}
